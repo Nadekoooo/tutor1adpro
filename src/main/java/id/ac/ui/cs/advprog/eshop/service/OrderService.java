a5daef3bd26d925298278f7929a98dc7e1ffc340
Nadekoooo <christianyudistira09@gmail.com>
2025-03-07 15:44:54 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
@@ -1,18 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
-import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-// OrderService Interface
 public interface OrderService {
     Order createOrder(Order order);
     Order updateStatus(String orderId, String status);
     Order findById(String orderId);
     List<Order> findAllByAuthor(String author);
 }
-
-
