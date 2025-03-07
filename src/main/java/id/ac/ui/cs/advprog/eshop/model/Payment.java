7cce633b61f9db13f75af48d199b34f0ba0ec0b6
Nadekoooo <christianyudistira09@gmail.com>
2025-03-07 16:08:31 +0700

    [RED] Add Payment model skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,4 +1,36 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+
+import java.util.Map;
+
 public class Payment {
+
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = null; // or some default, but not the real logic yet
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Map<String, String> getPaymentData() {
+        return paymentData;
+    }
 }
+
--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentTest.java
@@ -1,5 +1,6 @@
-package your.package.model;
+package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
